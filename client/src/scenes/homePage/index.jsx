6a0250256151bebbd945d41ed4ffc17d05de427a
wigzen <wigzen18@gmail.com>
2023-04-15 10:16:42 +0530

    Added Login page, Signup Page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Navbar from '../navbar/index'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <Box>
+        <Navbar />
+      </Box>
+    </div>
+  )
 }
 
 export default HomePage
